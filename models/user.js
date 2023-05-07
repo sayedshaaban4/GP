9229754ab80d7debbf2a6c15347bf71d0aa586d5
Sayed Shaaban <sayedshaabansayed@gmail.com>
2023-05-07 16:39:10 +0300

    added files

--- models/user.js
+++ models/user.js
@@ -5,16 +5,49 @@ const UserSchema = mongoose.Schema({
         required : true,
         trim : true,
         type : String,
-    }, 
+    },   
     password : {
         required : true,
         type : String,
-        // validate : {
-        //     validator : (value) => {
-        //         return value.length > 8;
-        //     },
-        //     message: 'your password is too short!',
-        // },
+    },
+    email :{
+        required : true,
+        trim : true,
+        type : String,
+        validate : {
+            validator : (value) => {
+            const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;      
+            },
+            message: 'your Email is invalid!',
+        },
+    },
+    phone: {
+        required : true,
+        trim : true,
+        type : String,
+        validate : {
+            validator : (value) => {
+                return value.length === 11;
+            },
+            message: 'your National is invalid!',
+        },
+    },
+    licence : {
+        required : true,
+        trim : true,
+        type : String,
+
+    },
+    NationalID : {
+        required : true,
+        trim : true,
+        type : String,
+        validate : {
+            validator : (value) => {
+                return value.length === 14;
+            },
+            message: 'your National is invalid!',
+        },
     },
     type : {
         type : String,
