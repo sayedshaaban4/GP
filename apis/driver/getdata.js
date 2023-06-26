3e558face720d133d428326491ab44098b4ae884
kambosha <>
2023-06-26 21:22:05 +0300

    v8

--- apis/driver/getdata.js
+++ apis/driver/getdata.js
@@ -5,7 +5,8 @@ const getData = express.Router();
 
 getData.get('/api/driver/getdata' , token , async (req , res) => {
     const user = await User.findById(req.user);
-    res.json({...user._doc , token : req.token});
+    const result = await User.find({userName : user.userName},{_id : 0 , password : 0 , type : 0 , __v : 0 , reportsValue : 0 , reportsCount : 0});
+    res.json({result});
 });
 
 module.exports = getData;
