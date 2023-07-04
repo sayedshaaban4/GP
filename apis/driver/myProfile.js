4b982049f618a23c7dcb895dcec5620f8c3b48b8
kambosha <61192867+sayedshaaban4@users.noreply.github.com>
2023-07-04 16:54:51 +0300

    solve update issue

--- apis/driver/myProfile.js
+++ apis/driver/myProfile.js
@@ -7,7 +7,7 @@ myProfileRouter.get('/api/driver/my-profile' , token , async (req , res) => {
     try{
         const user = await User.findById(req.user);
         const profileInfo = await User.find( {userName : user.userName} , 
-                                    {_id : 0 , password : 0 , type : 0 , __v : 0 , reportsValue : 0 , reportsCount : 0});
+                            {_id : 0 , password : 0 , type : 0 , __v : 0 , reportsValue : 0 , reportsCount : 0});
         res.json({profileInfo});
     }catch (e) {
         res.status(500).json({error: e}) ;
